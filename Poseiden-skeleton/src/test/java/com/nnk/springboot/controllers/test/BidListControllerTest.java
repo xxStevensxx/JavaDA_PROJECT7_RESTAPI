8441629829debb6e6fa6186efd855e88b5459e62
xxStevensxx <29457062+xxStevensxx@users.noreply.github.com>
2021-10-07 16:54:00 +0200

    Ajout des annotations spring pour les testws

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/controllers/test/BidListControllerTest.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/controllers/test/BidListControllerTest.java
@@ -9,16 +9,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.hamcrest.Matchers;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.nnk.springboot.domain.BidList;
 import com.nnk.springboot.repositories.BidListRepository;
 
 
-
-
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class BidListControllerTest {
 	
 	
